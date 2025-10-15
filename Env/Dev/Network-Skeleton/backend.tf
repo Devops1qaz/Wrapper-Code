05236e33d1a8d79fafe502f5407cfc14112db43b
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-10-15 17:49:06 +0530

    Update backend.tf

--- Env/Dev/Network-Skeleton/backend.tf
+++ Env/Dev/Network-Skeleton/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "otms-my-bucket"
     key     = "env/dev/network_skeleton/terraform.tfstate"
-    region  = "ap-south-1"
+    region  = "us-east-1"
     encrypt = true
   }
 }
